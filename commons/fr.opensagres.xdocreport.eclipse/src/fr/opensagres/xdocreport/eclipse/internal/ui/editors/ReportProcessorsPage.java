47c803f26f9289f251e6f7465f8cf68e3163527a
angelo.zerr@gmail.com <angelo.zerr@gmail.com>
2011-09-16 00:48:32 +0200

    Initialize Master detail.

--- commons/fr.opensagres.xdocreport.eclipse/src/fr/opensagres/xdocreport/eclipse/internal/ui/editors/ReportProcessorsPage.java
+++ commons/fr.opensagres.xdocreport.eclipse/src/fr/opensagres/xdocreport/eclipse/internal/ui/editors/ReportProcessorsPage.java
@@ -1,7 +1,9 @@
 package fr.opensagres.xdocreport.eclipse.internal.ui.editors;
 
+import org.eclipse.ui.forms.IManagedForm;
 import org.eclipse.ui.forms.editor.FormEditor;
 import org.eclipse.ui.forms.editor.FormPage;
+import org.eclipse.ui.forms.widgets.ScrolledForm;
 
 import fr.opensagres.xdocreport.eclipse.internal.Messages;
 
@@ -9,8 +11,20 @@ public class ReportProcessorsPage extends FormPage {
 
 	public static final String PAGE_ID = "processors";
 	
+	private final ScrolledPropertiesBlock block;
+	
 	public ReportProcessorsPage(FormEditor editor) {
 		super(editor, PAGE_ID, Messages.ReportProcessorsPage_title);
+		block = new ScrolledPropertiesBlock(this);
+	}	
+	
+	protected void createFormContent(final IManagedForm managedForm) {
+		final ScrolledForm form = managedForm.getForm();
+		//FormToolkit toolkit = managedForm.getToolkit();
+		//form.setText(Messages.getString("MasterDetailsPage.title")); //$NON-NLS-1$
+		//form.setBackgroundImage(FormArticlePlugin.getDefault().getImage(
+		//		FormArticlePlugin.IMG_FORM_BG));
+		block.createContent(managedForm);
 	}
 
 }
