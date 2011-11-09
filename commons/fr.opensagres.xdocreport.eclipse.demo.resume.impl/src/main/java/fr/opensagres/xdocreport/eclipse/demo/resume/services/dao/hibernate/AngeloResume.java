ba84e542381fc61ec1e57a429dd8c6fcd0aca2a1
pascal leclercq <pascal.leclercq@gmail.com>
2011-11-10 00:50:48 +0100

    merge...

--- commons/fr.opensagres.xdocreport.eclipse.demo.resume.impl/src/main/java/fr/opensagres/xdocreport/eclipse/demo/resume/services/dao/hibernate/AngeloResume.java
+++ commons/fr.opensagres.xdocreport.eclipse.demo.resume.impl/src/main/java/fr/opensagres/xdocreport/eclipse/demo/resume/services/dao/hibernate/AngeloResume.java
@@ -17,25 +17,9 @@ import fr.opensagres.xdocreport.eclipse.demo.resume.services.impl.DateUtils;
 public class AngeloResume extends Resume {
 
 	public AngeloResume() {
-		// resume
-		super.setId(ResumeServiceImpl.currentId++);
-		try {
-			super.setPictureAsStream(Resume.class
-					.getResourceAsStream("AngeloZERR.jpg"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		super.setTitle("Ingénieur Etude JEE/Eclipse RCP");
-
-		
-		// Person
+		// Angelo
 		NaturalPerson person = new NaturalPerson();
-<<<<<<< HEAD:commons/fr.opensagres.xdocreport.eclipse.demo.resume.impl/src/main/java/fr/opensagres/xdocreport/eclipse/demo/resume/services/dao/hibernate/AngeloResume.java
 		person.setId(ResumeDaoHibernate.currentId++);
-=======
-		super.setOwner(person);
-		person.setId(ResumeServiceImpl.currentId++);
->>>>>>> 60349a5d501240c66f99c92cd01bcefc6ace937c:commons/fr.opensagres.xdocreport.eclipse.demo.resume.impl/src/main/java/fr/opensagres/xdocreport/eclipse/demo/resume/services/impl/AngeloResume.java
 		person.setFirstName("Angelo");
 		person.setLastName("ZERR");
 		person.setEmail("angelo.zerr@gmail.com");
@@ -44,7 +28,6 @@ public class AngeloResume extends Resume {
 		} catch (ParseException e1) {
 			e1.printStackTrace();
 		}
-<<<<<<< HEAD:commons/fr.opensagres.xdocreport.eclipse.demo.resume.impl/src/main/java/fr/opensagres/xdocreport/eclipse/demo/resume/services/dao/hibernate/AngeloResume.java
 		super.setId(ResumeDaoHibernate.currentId++);
 		super.setOwner(person);
 		try {
@@ -59,8 +42,6 @@ public class AngeloResume extends Resume {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-=======
->>>>>>> 60349a5d501240c66f99c92cd01bcefc6ace937c:commons/fr.opensagres.xdocreport.eclipse.demo.resume.impl/src/main/java/fr/opensagres/xdocreport/eclipse/demo/resume/services/impl/AngeloResume.java
 
 		// Address
 		Address address = new Address();
@@ -71,7 +52,6 @@ public class AngeloResume extends Resume {
 		address.setCity("Saint Paul 3 Châteaux");
 		address.setZipCode("26130");
 
-<<<<<<< HEAD:commons/fr.opensagres.xdocreport.eclipse.demo.resume.impl/src/main/java/fr/opensagres/xdocreport/eclipse/demo/resume/services/dao/hibernate/AngeloResume.java
 		// Diplomas
 		Set<Education> educations = new HashSet<Education>();
 
@@ -83,37 +63,6 @@ public class AngeloResume extends Resume {
 		diploma.setLabel("Diplôme d'ingénieur en informatique");
 		diploma.setInstitute("INSA de Lyon");
 		educations.add(diploma);
-=======
-		// Educations
-		Set<Education> educations = new HashSet<Education>();
-		super.setEducations(educations);
-
-		Education education = null;
-
-		// Education 1
-		education = new Education();
-		education.setId(ResumeServiceImpl.currentId++);
-		try {
-			education.setDate(DateUtils.toDate("30/06/2001", DateUtils.FRENCH_PATTERN));
-		} catch (ParseException e1) {
-			e1.printStackTrace();
-		}
-		education.setLabel("Diplôme d'ingénieur en informatique");
-		education.setInstitute("INSA de Lyon");
-		educations.add(education);
-
-		// Education 2
-		education = new Education();
-		education.setId(ResumeServiceImpl.currentId++);
-		try {
-			education.setDate(DateUtils.toDate("30/06/1996", DateUtils.FRENCH_PATTERN));
-		} catch (ParseException e1) {
-			e1.printStackTrace();
-		}
-		education.setLabel("BAC S option Physique C");
-		education.setInstitute("Lycée Georges de la Tour de Nancy");
-		educations.add(education);
->>>>>>> 60349a5d501240c66f99c92cd01bcefc6ace937c:commons/fr.opensagres.xdocreport.eclipse.demo.resume.impl/src/main/java/fr/opensagres/xdocreport/eclipse/demo/resume/services/impl/AngeloResume.java
 
 		// Experiences
 		Set<Experience> experiences = new HashSet<Experience>();
@@ -127,11 +76,6 @@ public class AngeloResume extends Resume {
 		experience.setMission("Conception / Développement");
 		experience
 				.setDetail("Mise en place de l'application <b>WEB de diffusion</b> (qui sera accessible dans les accueils des CAF) qui permet de publier les documents XML produits par l'application WEB de production.");
-		try {
-			experience.setStartDate(DateUtils.toDate("01/04/2009", DateUtils.FRENCH_PATTERN));
-		} catch (ParseException e1) {
-			e1.printStackTrace();
-		}
 		experiences.add(experience);
 
 		// Experience 2
@@ -141,12 +85,6 @@ public class AngeloResume extends Resume {
 		experience.setMission("Conception / Développement");
 		experience
 				.setDetail("Conception et développement de fonctionnalités dans le  module VENTES/ACHATS et COMPTABILITE de l'ERP agrolimentaire AgroV3 de <b>INFOLOGIC</b>. Cet ERP est basé sur les technologies d'<b>Eclipse SWT et JFace</b>.");
-		try {
-			experience.setStartDate(DateUtils.toDate("01/08/2007", DateUtils.FRENCH_PATTERN));
-			experience.setEndDate(DateUtils.toDate("31/03/2009", DateUtils.FRENCH_PATTERN));
-		} catch (ParseException e1) {
-			e1.printStackTrace();
-		}
 		experiences.add(experience);
 
 		// Hobbies
--- commons/fr.opensagres.xdocreport.eclipse.demo.resume.impl/src/main/java/fr/opensagres/xdocreport/eclipse/demo/resume/services/impl/ResumeServiceImpl.java
+++ commons/fr.opensagres.xdocreport.eclipse.demo.resume.impl/src/main/java/fr/opensagres/xdocreport/eclipse/demo/resume/services/impl/ResumeServiceImpl.java
@@ -1,168 +1,182 @@
 package fr.opensagres.xdocreport.eclipse.demo.resume.services.impl;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 
-import fr.opensagres.xdocreport.eclipse.demo.resume.domain.core.Address;
-import fr.opensagres.xdocreport.eclipse.demo.resume.domain.core.NaturalPerson;
-import fr.opensagres.xdocreport.eclipse.demo.resume.domain.hr.Education;
-import fr.opensagres.xdocreport.eclipse.demo.resume.domain.hr.Experience;
-import fr.opensagres.xdocreport.eclipse.demo.resume.domain.hr.Hobby;
 import fr.opensagres.xdocreport.eclipse.demo.resume.domain.hr.Resume;
 import fr.opensagres.xdocreport.eclipse.demo.resume.services.ResumeService;
+import fr.opensagres.xdocreport.eclipse.demo.resume.services.dao.ResumeDao;
 
 public class ResumeServiceImpl implements ResumeService {
 
-	private static final Map<Long, Resume> resumes;
-	static long currentId = 0;
-	static {
-		resumes = new HashMap<Long, Resume>();
-
-		// Angelo
-		Resume angeloResume = new AngeloResume();
-		resumes.put(angeloResume.getId(), angeloResume);
-
-		// Pascal
-		Resume pascalResume = new PascalResume();
-		resumes.put(pascalResume.getId(), pascalResume);
-
-		// Amine
-		Resume amineResume = new AmineResume();
-		resumes.put(amineResume.getId(), amineResume);
+	private ResumeDao resumeDao;
+	
+	public void setResumeDao(ResumeDao resumeDao) {
+		this.resumeDao = resumeDao;
 	}
 
 	public Collection<Resume> findAll() {
-		return resumes.values();
+		// TODO Auto-generated method stub
+		return resumeDao.findAll();
 	}
 
 	public Resume findById(long id) {
-		Resume Resume = resumes.get(id);
-		if (Resume != null) {
-			return clone(Resume);
-		}
-		return resumes.get(id);
+		// TODO Auto-generated method stub
+		return resumeDao.findById(id);
 	}
 
 	public Resume save(Resume resume) {
-		if (resume.getId() == null) {
-			resume.setId(currentId++);
-		}
-		resumes.put(resume.getId(), resume);
-		return clone(resume);
+		// TODO Auto-generated method stub
+		return resumeDao.save(resume);
 	}
-
-	private Resume clone(Resume resume) {
-		NaturalPerson person = resume.getOwner();
-		NaturalPerson newPerson = clone(person);
-
-		Resume newResume = new Resume();
-		newResume.setId(resume.getId());
-		newResume.setOwner(newPerson);
-		newResume.setPicture(resume.getPicture());
-		newResume.setTitle(resume.getTitle());
-		
-		// Educations
-		Set<Education> educations = resume.getEducations();
-		if (educations != null) {
-			Set<Education> newEducations = new HashSet<Education>();
-			for (Education education : educations) {
-				newEducations.add(clone(education));
-			}
-			newResume.setEducations(newEducations);
-		}
-
-		// Experiences
-		Set<Experience> experiences = resume.getExperiences();
-		if (experiences != null) {
-			Set<Experience> newExperiences = new HashSet<Experience>();
-			for (Experience experience : experiences) {
-				newExperiences.add(clone(experience));
-			}
-			newResume.setExperiences(newExperiences);
-		}
-
-		// Hobbies
-		Set<Hobby> hobbies = resume.getHobbies();
-		if (hobbies != null) {
-			Set<Hobby> newHobbies = new HashSet<Hobby>();
-			for (Hobby hobby : hobbies) {
-				newHobbies.add(clone(hobby));
-			}
-			newResume.setHobbies(newHobbies);
-		}
-
-		return newResume;
-	}
-
-	private NaturalPerson clone(NaturalPerson person) {
-		NaturalPerson newPerson = new NaturalPerson();
-		newPerson.setId(person.getId());
-		newPerson.setLastName(person.getLastName());
-		newPerson.setFirstName(person.getFirstName());
-		newPerson.setBirthDate(person.getBirthDate());
-		newPerson.setEmail(person.getEmail());
-		newPerson.setAddress(clone(person.getAddress()));
-		return newPerson;
-	}
-
-	private Experience clone(Experience experience) {
-		Experience newExperience = new Experience();
-		Long id = experience.getId();
-		if (id == null) {
-			id = currentId++;
-		}
-		newExperience.setId(id);
-		newExperience.setTitle(experience.getTitle());
-		newExperience.setDetail(experience.getDetail());
-		newExperience.setMission(experience.getMission());
-		newExperience.setEndDate(experience.getEndDate());
-		newExperience.setStartDate(experience.getStartDate());
-		return newExperience;
-	}
-
-	private Education clone(Education education) {
-		Education newEducation = new Education();
-		Long id = education.getId();
-		if (id == null) {
-			id = currentId++;
-		}
-		newEducation.setId(id);
-		newEducation.setDate(education.getDate());
-		newEducation.setLabel(education.getLabel());
-		newEducation.setInstitute(education.getInstitute());
-		return newEducation;
-	}
-
-	private Hobby clone(Hobby hobby) {
-		Hobby newHobby = new Hobby();
-		Long id = hobby.getId();
-		if (id == null) {
-			id = currentId++;
-		}
-		newHobby.setId(id);
-		newHobby.setLabel(hobby.getLabel());
-		return newHobby;
-	}
-
-	private Address clone(Address address) {
-		if (address == null) {
-			return null;
-		}
-		Address newAddress = new Address();
-		Long id = address.getId();
-		if (id == null) {
-			id = currentId++;
-		}
-		newAddress.setId(id);
-		newAddress.setCity(address.getCity());
-		newAddress.setCountry(address.getCountry());
-		newAddress.setFax(address.getFax());
-		newAddress.setStreet(address.getStreet());
-		newAddress.setTelephone(address.getTelephone());
-		newAddress.setZipCode(address.getZipCode());
-		return newAddress;
-	}
+	
+//	
+//	private static final Map<Long, Resume> resumes;
+//	static long currentId = 0;
+//	static {
+//		resumes = new HashMap<Long, Resume>();
+//
+//		// Angelo
+//		Resume angeloResume = new AngeloResume();
+//		resumes.put(angeloResume.getId(), angeloResume);
+//
+//		// Pascal
+//		Resume pascalResume = new PascalResume();
+//		resumes.put(pascalResume.getId(), pascalResume);
+//
+//		// Amine
+//		Resume amineResume = new AmineResume();
+//		resumes.put(amineResume.getId(), amineResume);
+//	}
+//
+//	public Collection<Resume> findAll() {
+//		return resumes.values();
+//	}
+//
+//	public Resume findById(long id) {
+//		Resume Resume = resumes.get(id);
+//		if (Resume != null) {
+//			return clone(Resume);
+//		}
+//		return resumes.get(id);
+//	}
+//
+//	public Resume save(Resume resume) {
+//		if (resume.getId() == null) {
+//			resume.setId(currentId++);
+//		}
+//		resumes.put(resume.getId(), resume);
+//		return clone(resume);
+//	}
+//
+//	private Resume clone(Resume resume) {
+//		NaturalPerson person = resume.getOwner();
+//		NaturalPerson newPerson = clone(person);
+//
+//		Resume newResume = new Resume();
+//		newResume.setId(resume.getId());
+//		newResume.setOwner(newPerson);
+//		newResume.setPicture(resume.getPicture());
+//		newResume.setTitle(resume.getTitle());
+//		
+//		// Educations
+//		Set<Education> educations = resume.getEducations();
+//		if (educations != null) {
+//			Set<Education> newEducations = new HashSet<Education>();
+//			for (Education education : educations) {
+//				newEducations.add(clone(education));
+//			}
+//			newResume.setEducations(newEducations);
+//		}
+//
+//		// Experiences
+//		Set<Experience> experiences = resume.getExperiences();
+//		if (experiences != null) {
+//			Set<Experience> newExperiences = new HashSet<Experience>();
+//			for (Experience experience : experiences) {
+//				newExperiences.add(clone(experience));
+//			}
+//			newResume.setExperiences(newExperiences);
+//		}
+//
+//		// Hobbies
+//		Set<Hobby> hobbies = resume.getHobbies();
+//		if (hobbies != null) {
+//			Set<Hobby> newHobbies = new HashSet<Hobby>();
+//			for (Hobby hobby : hobbies) {
+//				newHobbies.add(clone(hobby));
+//			}
+//			newResume.setHobbies(newHobbies);
+//		}
+//
+//		return newResume;
+//	}
+//
+//	private NaturalPerson clone(NaturalPerson person) {
+//		NaturalPerson newPerson = new NaturalPerson();
+//		newPerson.setId(person.getId());
+//		newPerson.setLastName(person.getLastName());
+//		newPerson.setFirstName(person.getFirstName());
+//		newPerson.setBirthDate(person.getBirthDate());
+//		newPerson.setEmail(person.getEmail());
+//		newPerson.setAddress(clone(person.getAddress()));
+//		return newPerson;
+//	}
+//
+//	private Experience clone(Experience experience) {
+//		Experience newExperience = new Experience();
+//		Long id = experience.getId();
+//		if (id == null) {
+//			id = currentId++;
+//		}
+//		newExperience.setId(id);
+//		newExperience.setTitle(experience.getTitle());
+//		newExperience.setDetail(experience.getDetail());
+//		newExperience.setMission(experience.getMission());
+//		newExperience.setEndDate(experience.getEndDate());
+//		newExperience.setStartDate(experience.getStartDate());
+//		return newExperience;
+//	}
+//
+//	private Education clone(Education education) {
+//		Education newEducation = new Education();
+//		Long id = education.getId();
+//		if (id == null) {
+//			id = currentId++;
+//		}
+//		newEducation.setId(id);
+//		newEducation.setDate(education.getDate());
+//		newEducation.setLabel(education.getLabel());
+//		newEducation.setInstitute(education.getInstitute());
+//		return newEducation;
+//	}
+//
+//	private Hobby clone(Hobby hobby) {
+//		Hobby newHobby = new Hobby();
+//		Long id = hobby.getId();
+//		if (id == null) {
+//			id = currentId++;
+//		}
+//		newHobby.setId(id);
+//		newHobby.setLabel(hobby.getLabel());
+//		return newHobby;
+//	}
+//
+//	private Address clone(Address address) {
+//		if (address == null) {
+//			return null;
+//		}
+//		Address newAddress = new Address();
+//		Long id = address.getId();
+//		if (id == null) {
+//			id = currentId++;
+//		}
+//		newAddress.setId(id);
+//		newAddress.setCity(address.getCity());
+//		newAddress.setCountry(address.getCountry());
+//		newAddress.setFax(address.getFax());
+//		newAddress.setStreet(address.getStreet());
+//		newAddress.setTelephone(address.getTelephone());
+//		newAddress.setZipCode(address.getZipCode());
+//		return newAddress;
+//	}
 }
