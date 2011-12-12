7e862981913af97510594619efb02a829e6fa037
angelo.zerr@gmail.com <angelo.zerr@gmail.com>
2011-12-12 13:41:14 +0100

    Add ASF License.

--- commons/org.eclipse.nebula.widgets.picture/src/main/java/org/eclipse/nebula/widgets/picture/internal/ByteArrayOutputStream.java
+++ commons/org.eclipse.nebula.widgets.picture/src/main/java/org/eclipse/nebula/widgets/picture/internal/ByteArrayOutputStream.java
@@ -1,7 +1,3 @@
-package org.eclipse.nebula.widgets.picture.internal;
-
-
-	 
 /*
 * Licensed to the Apache Software Foundation (ASF) under one or more
 * contributor license agreements.  See the NOTICE file distributed with
@@ -18,6 +14,7 @@ package org.eclipse.nebula.widgets.picture.internal;
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
+package org.eclipse.nebula.widgets.picture.internal;
 
 import java.io.IOException;
 import java.io.InputStream;
