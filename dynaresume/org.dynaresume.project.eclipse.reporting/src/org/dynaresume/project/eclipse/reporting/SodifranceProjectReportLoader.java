fdd7754593f12072cc95d16f7027f4993173c1df
angelo.zerr@gmail.com <angelo.zerr@gmail.com>
2011-12-15 13:00:21 +0100

    Add sodifrance project docx.

---
+++ dynaresume/org.dynaresume.project.eclipse.reporting/src/org/dynaresume/project/eclipse/reporting/SodifranceProjectReportLoader.java
@@ -0,0 +1,30 @@
+package org.dynaresume.project.eclipse.reporting;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import fr.opensagres.xdocreport.eclipse.reporting.core.ReportException;
+import fr.opensagres.xdocreport.eclipse.reporting.xdocreport.core.XDocReportLoader;
+import fr.opensagres.xdocreport.template.TemplateEngineKind;
+import fr.opensagres.xdocreport.template.formatter.FieldsMetadata;
+import fr.opensagres.xdocreport.template.textstyling.SyntaxKind;
+
+public class SodifranceProjectReportLoader extends XDocReportLoader {
+
+	public InputStream doGetSourceStream() throws IOException, ReportException {
+		return ProjectReportProcessor.class.getResourceAsStream("SodifranceProject.docx");
+	}
+
+	@Override
+	public String getTemplateEngineKind() {
+		return TemplateEngineKind.Velocity.name();
+	}
+
+	@Override
+	public FieldsMetadata getFieldsMetadata() {
+		FieldsMetadata metadata = new FieldsMetadata();
+		metadata.addFieldAsTextStyling("project.Description", SyntaxKind.Html);
+		return metadata;
+	}
+
+}
