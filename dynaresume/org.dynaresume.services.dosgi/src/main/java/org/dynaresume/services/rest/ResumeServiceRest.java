9c33c0085db845e4c780c1277eb27863dd3bfa19
pascal leclercq <pascal.leclercq@gmail.com>
2012-01-19 12:18:26 +0100

    Les tests de service REST commencent à marchouiller....

--- dynaresume/org.dynaresume.services.dosgi/src/main/java/org/dynaresume/services/rest/ResumeServiceRest.java
+++ dynaresume/org.dynaresume.services.dosgi/src/main/java/org/dynaresume/services/rest/ResumeServiceRest.java
@@ -9,10 +9,19 @@ import org.dynaresume.domain.hr.Resume;
 @Path("/resume/")
 public class ResumeServiceRest {
 	
+	
+	@GET
+	@Path("test")
+	@Produces("text/plain")
+	public String hello(){
+		return "Hello !!!";
+	}
+	
+	
 	@GET
 	@Path("findAll")
 	@Produces("application/json")
-	Iterable<Resume> findAll(){
+	public Iterable<Resume> findAll(){
 		
 		List<Resume> resumes=new ArrayList<Resume>();
 		resumes.add(new Resume());
--- dynaresume/org.dynaresume.services.impl.test/src/test/java/org/dynaresume/services/dosgi/JAXRSOSGiUnitTest.java
+++ dynaresume/org.dynaresume.services.impl.test/src/test/java/org/dynaresume/services/dosgi/JAXRSOSGiUnitTest.java
@@ -2,7 +2,7 @@ package org.dynaresume.services.dosgi;
 
 
 import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
-import static org.ops4j.pax.exam.CoreOptions.profile;
+import static org.ops4j.pax.exam.CoreOptions.*;
 import static org.ops4j.pax.exam.OptionUtils.combine;
 
 import java.io.IOException;
@@ -14,87 +14,53 @@ import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.TimeoutException;
 import org.ops4j.pax.exam.junit.Configuration;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.ops4j.pax.exam.options.ProvisionOption;
 import org.ops4j.pax.exam.spi.PaxExamRuntime;
 
 //starts this class  as an "application" (with a "main").
 @RunWith(JUnit4TestRunner.class)
-public class JAXRSOSGiUnitTest {
+public class JAXRSOSGiUnitTest extends AbstractJAXRSOSGiUnitTest {
 
 	@Configuration
-	public static Option[] config() {
+	public  Option[] config() {
 		return CoreOptions.options(CoreOptions.cleanCaches(),
-				CoreOptions.profile("compendium"),
-				//CoreOptions.profile("compendium"),
-				CoreOptions.mavenBundle().groupId("org.apache.cxf.dosgi")
-						.artifactId("cxf-dosgi-ri-singlebundle-distribution")
-						.version("1.2"),
-						// ***************** Spring dependencies ********************
-						mavenBundle().groupId("org.aopalliance")
-								.artifactId("com.springsource.org.aopalliance")
-								.version("1.0.0"),
-						mavenBundle().groupId("org.springframework")
-								.artifactId("org.springframework.aop")
-								.version("3.0.6.RELEASE"),
-						mavenBundle().groupId("org.springframework")
-								.artifactId("org.springframework.beans")
-								.version("3.0.6.RELEASE"),
-						mavenBundle().groupId("org.springframework")
-								.artifactId("org.springframework.context")
-								.version("3.0.6.RELEASE"),
 
-						mavenBundle().groupId("org.springframework")
-								.artifactId("org.springframework.core")
-								.version("3.0.6.RELEASE"),
-						mavenBundle().groupId("org.springframework")
-								.artifactId("org.springframework.transaction")
-								.version("3.0.6.RELEASE"),
-						mavenBundle().groupId("org.springframework")
-								.artifactId("org.springframework.orm")
-								.version("3.0.6.RELEASE"),
-						mavenBundle().groupId("org.springframework")
-								.artifactId("org.springframework.jdbc")
-								.version("3.0.6.RELEASE"),
+				// Run this test under Felix.
+				CoreOptions.frameworks(CoreOptions.felix()),
+		        composite(infra()),
+				
+				composite(brails()),
+				composite(cfxdosgi()),
+				//TODO:
+				//bundle("file:multibundle/apache-cxf-dosgi-ri-1.2/dosgi_bundles/spring-osgi-extender-1.2.0.jar").startLevel(6),
+				systemProperty("org.osgi.framework.startlevel.beginning").value("" + 20),
+				
 
-						//
-						mavenBundle().groupId("org.springframework")
-								.artifactId("org.springframework.asm")
-								.version("3.0.6.RELEASE"),
-						mavenBundle().groupId("org.springframework")
-								.artifactId("org.springframework.expression")
-								.version("3.0.6.RELEASE"),
-
-						// ***************** Gemini dependencies ********************
-						mavenBundle().groupId("org.eclipse.gemini.blueprint")
-								.artifactId("gemini-blueprint-core")
-								.version("1.0.0.RELEASE"),
-
-						mavenBundle().groupId("org.eclipse.gemini.blueprint")
-								.artifactId("gemini-blueprint-io")
-								.version("1.0.0.RELEASE"),
-
-						mavenBundle().groupId("org.eclipse.gemini.blueprint")
-								.artifactId("gemini-blueprint-extender")
-								.version("1.0.0.RELEASE").startLevel(5),
-						
-				// The following two bundles start the greeter demo which
-				// registers a remote service
-
-				CoreOptions.mavenBundle("org.codehaus.jackson",
-						"jackson-jaxrs", "1.9.0"), CoreOptions.mavenBundle(
-						"org.codehaus.jackson", "jackson-core-asl", "1.9.0"),
-				CoreOptions.mavenBundle("org.codehaus.jackson",
-						"jackson-mapper-asl", "1.9.0"));
+				// For debugging...
+				// opts.add(PaxRunnerOptions.vmOption(
+				// "-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=6006" ));
+				// opts.add(CoreOptions.waitForFrameworkStartup());
+				// end debugging section.
+				 // end debugging section.
+				
+		        workingDirectory("tmp/felix"));
 	}
 
 	public static void main(String[] args) throws TimeoutException, IOException {
 		PaxExamRuntime.createContainer(
-				PaxExamRuntime.createTestSystem(combine(JAXRSOSGiUnitTest.config(),
+				PaxExamRuntime.createTestSystem(combine(new JAXRSOSGiUnitTest().config(),
 						profile("web")))).start();
 	}
 	
 	@Test
 	public void test(){
 		System.out.println("coucou");
+	}
+
+	@Override
+	protected void configureStartupLevel(ProvisionOption[] options) {
+		
+		
 	}
 
 }
