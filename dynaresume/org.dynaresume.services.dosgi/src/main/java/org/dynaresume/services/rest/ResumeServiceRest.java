37668740c6bf898803f5e93e061b8132789b05a0
pascal leclercq <pascal.leclercq@gmail.com>
2012-01-23 00:15:59 +0100

    prepare save method

--- dynaresume/org.dynaresume.services.dosgi/src/main/java/org/dynaresume/services/rest/ResumeServiceRest.java
+++ dynaresume/org.dynaresume.services.dosgi/src/main/java/org/dynaresume/services/rest/ResumeServiceRest.java
@@ -3,7 +3,9 @@ package org.dynaresume.services.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -12,9 +14,6 @@ import org.dynaresume.domain.hr.Resume;
 
 @Path("/resume/")
 public class ResumeServiceRest {
-	
-	
-	
 	
 	@GET
 	@Path("findAll")
@@ -37,6 +36,8 @@ public class ResumeServiceRest {
 		return result;
 	}
 
+	@POST
+	@Consumes("application/json")
 	public Resume save(Resume resume){
 		return resume;
 	}
